ac2e816518046d481ff3ed4a9b704bd5388355a8
Shottle <ek_marveeva@mail.ru>
2022-01-18 14:51:11 +0300

    v1

---
+++ game_kate_2.py
@@ -0,0 +1,39 @@
+"""Predict the number!"""
+
+import numpy as np
+
+def random_predict(number: int = 1) -> int:
+
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла, если угадали
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывается число
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    
+    count_ls = []
+    np.random.seed(1) #фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) #Загадали список чисел
+    
+    for number in random_array:
+        count_ls.append(random_predict(number))
+    
+    score = int(np.mean(count_ls))
+    print(f'Ваш алгоритм в среднем угадывает число за: {score} попыток')
+    return(score)
+
+score_game(random_predict)
+if __name__ == '__main__':
